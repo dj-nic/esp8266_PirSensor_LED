65610f59932f9841d75f96eb3f65b2d9f1fdb287
dj-nic <70818245+dj-nic@users.noreply.github.com>
2020-11-19 21:55:31 +0100

    Add files via upload

---
+++ Bewegungsmelder_PIR_LED.ino
@@ -0,0 +1,36 @@
+
+const int PIR = D2;
+int PIRState = 0;
+
+void setup() {
+  pinMode(PIR, INPUT);
+  pinMode(D3, OUTPUT);
+  Serial.begin (115200);
+  delay(2000);
+  Serial.println (" ");
+  Serial.println ("ON");
+
+  digitalWrite(D2, LOW); // LED aus
+}
+
+void loop() {
+
+  PIRState = digitalRead(PIR);
+
+  if (PIRState == HIGH) {
+    Serial.println (PIR, DEC);
+    Serial.println ("triggered");
+    digitalWrite(D3, HIGH) ;  // LED an
+    delay (100); // delay 0,1 sekunden
+    digitalWrite(D3, LOW); // LED aus
+    delay(100);
+  } else { // oder
+    Serial.println (PIR, DEC);
+    Serial.println ("active");
+    digitalWrite(D3, HIGH);
+    delay(50);
+    digitalWrite(D3, LOW);
+    delay(2000);
+
+  }
+}
